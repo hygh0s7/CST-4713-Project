85afb182b72c7ae8cc47670aaeaca1cb81eb793a
Klodjan2391 <klodjan2391@gmail.com>
2019-04-23 16:25:19 -0400

    Fixed Error in CheckLogin

--- teamProj/src/servlets/CheckLogin.java
+++ teamProj/src/servlets/CheckLogin.java
@@ -5,7 +5,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 
-@WebServlet("CheckLogin")
+@WebServlet("/CheckLogin")
 public class CheckLogin extends HttpServlet {
 	
 	@Override
@@ -18,11 +18,8 @@ public class CheckLogin extends HttpServlet {
 		
 		boolean isInvalid = false;
 		
-<<<<<<< HEAD
+
 		if(studId == "" || studId == null ){
-=======
-		if( studId == "" || studId == null){
->>>>>>> 328f2ebeda737d84875c78fa6c0d0d54a08ccd2a
 			nullId = "Please Enter A Valid Student ID";
 			isInvalid = true;
 		}
